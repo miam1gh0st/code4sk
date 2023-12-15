f66dfc70b01bc6d3e3867634270d2ab8c9f869d0
miam1gh0st <1992584620@nbt.edu.cn>
2023-12-15 22:10:42 +0800

    refreash

--- c/sourcecode/朱森森带领的奇妙冒险/2022-2023第二学期单开班第1次月考/test.cpp
+++
@@ -1,8 +0,0 @@
-
-#include<stdio.h>
-int main(){
-int i=10, j=0;
-if (j=0)i++; else i--;
-	printf("%d",i);
-	return 0;
-}
---
+++ c/sourcecode/朱森森带领的奇妙冒险/C语言程序设计入门过关题集-顺序结构/Programming in C is fun!.cpp
@@ -0,0 +1,28 @@
+/*
+7-3 Programming in C is fun!
+分数 20
+作者 C课程组
+单位 浙江大学
+本题要求编写程序，输出一个短句“Programming in C is fun!”。
+
+输入格式:
+本题目没有输入。
+
+输出格式:
+在一行中输出短句“Programming in C is fun!”。
+
+代码长度限制
+16 KB
+时间限制
+400 ms
+内存限制
+64 MB
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    printf("Programming in C is fun!");
+    return 0;
+}
